b08ffd5adaee27171a29fe0de4d8e55988a8e3f5
Anish S. Shah <asshah4@users.noreply.github.com>
2025-05-12 21:26:16 -0600

    add images for septal at case

--- R/image_to_qmd.sh
+++ R/image_to_qmd.sh
@@ -3,7 +3,7 @@
 # Simple script to add all the images for a image based conference
 # Presumes that the names are all sequential/organized to some degree
 
-for i in $(ls -1tr *.{png,jpg,jpeg,JPG,JPEG,PNG} 2>/dev/null); do
+for i in $(ls -1tr *.{png,jpg,jpeg,gif,JPG,JPEG,PNG,GIF} 2>/dev/null); do
 	echo "## {background-image=\"$i\" background-size=\"contain\"}"
 	echo ""
 done >> case.qmd
